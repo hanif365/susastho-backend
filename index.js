e7ae5ee200dce084ea2be7bfd73ffdfb939a4548
hanif365 <m.a.hanifkhan64@gmail.com>
2021-12-12 05:30:47 +0600

    emergency info insert and get from DB

--- index.js
+++ index.js
@@ -27,6 +27,8 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 client.connect(err => {
     const doctorsCollection = client.db("susastho").collection("doctors");
     const appointmentCollection = client.db("susastho").collection("appointment");
+    const adminCollection = client.db("susastho").collection("admin");
+    const emergencyInfoCollection = client.db("susastho").collection("emergencyInfo");
 
 
     // Insert a doctor info in the DB
@@ -44,7 +46,7 @@ client.connect(err => {
     app.get('/doctors', (req, res) => {
         doctorsCollection.find()
             .toArray((err, doctorsInfo) => {
-                console.log("Doctors Info : ",doctorsInfo);
+                console.log("Doctors Info : ", doctorsInfo);
                 res.send(doctorsInfo);
             })
     })
@@ -64,8 +66,47 @@ client.connect(err => {
     app.get('/appointment', (req, res) => {
         appointmentCollection.find()
             .toArray((err, appointmentInfo) => {
-                console.log("Doctors Info : ",appointmentInfo);
+                console.log("Doctors Info : ", appointmentInfo);
                 res.send(appointmentInfo);
+            })
+    })
+
+    // Make admin
+    app.post('/addAdmin', (req, res) => {
+        const newAdmin = req.body;
+        adminCollection.insertOne(newAdmin)
+            .then(result => {
+                console.log('Inserted Count ', result.insertedCount)
+                res.send(result.insertedCount > 0)
+            })
+    })
+
+    // Check an user admin or not
+    app.post('/isAdmin', (req, res) => {
+        const email = req.body.email;
+        adminCollection.find({ email: email })
+            .toArray((err, admin) => {
+                res.send(admin.length > 0);
+            })
+    })
+
+    // Emergency Info insert into DB
+    app.post('/addemergencyinfo', (req, res) => {
+        const addEmergencyInfo = req.body;
+        console.log('Adding new Doctor', addEmergencyInfo);
+        emergencyInfoCollection.insertOne(addEmergencyInfo)
+            .then(result => {
+                console.log('Inserted Count ', result.insertedCount)
+                // res.send(result.insertedCount > 0)
+            })
+    })
+
+    // Get Emergency Info From DB
+    app.get('/emergencyInfo', (req, res) => {
+        emergencyInfoCollection.find()
+            .toArray((err, emergencyInfo) => {
+                console.log("Doctors Info : ", emergencyInfo);
+                res.send(emergencyInfo);
             })
     })
 
