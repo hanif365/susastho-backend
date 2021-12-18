1fb9a289604f1c676d5177569dd2800ee69511ce
hanif365 <m.a.hanifkhan64@gmail.com>
2021-12-18 12:13:39 +0600

    CORS Policy resolved for netlify

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ require('dotenv').config()
 const port = process.env.PORT || 5000
 
 app.use(cors({
-    origin: "http://localhost:3000"
+    origin: "https://susastho.netlify.app/"
 }));
 // app.use(cors());
 app.use(bodyParser.json());
