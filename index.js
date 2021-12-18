caf8b0e733471f690c418c2cc460958a570f7b19
hanif365 <m.a.hanifkhan64@gmail.com>
2021-12-19 02:58:19 +0600

    solve CORS error again

--- index.js
+++ index.js
@@ -10,7 +10,13 @@ require('dotenv').config()
 
 const port = process.env.PORT || 5000
 
-app.use(cors({ origin: true }));
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+// app.use(cors({ origin: true }));
 // app.use(cors());
 app.use(bodyParser.json());
 
