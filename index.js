e1605dc975e89d4d39e41106762ad4362575e8ba
hanif365 <m.a.hanifkhan64@gmail.com>
2021-12-19 04:18:48 +0600

    app.use rollback

--- index.js
+++ index.js
@@ -10,22 +10,17 @@ require('dotenv').config()
 
 const port = process.env.PORT || 5000
 
-// 'Access-Control-Allow-Origin': '*',
-//     'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept, Z-Key',
-//         'Content-Type': 'application/json',
-//             'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS'
-
-//             headers.add("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
-
-app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-    next();
-});
+// app.use(function (req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+//     next();
+// });
 
 // app.use(cors({ origin: true }));
-// app.use(cors());
+
+
+app.use(cors());
 app.use(bodyParser.json());
 
 
