7edef38d8e3a10bf47fe1f62ec25039ce30955dd
hanif365 <m.a.hanifkhan64@gmail.com>
2021-12-18 12:08:50 +0600

    CORS Policy resolved for localhost

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ require('dotenv').config()
 const port = process.env.PORT || 5000
 
 app.use(cors({
-    origin: "*"
+    origin: "http://localhost:3000"
 }));
 // app.use(cors());
 app.use(bodyParser.json());
