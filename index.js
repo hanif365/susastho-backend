4671a0eb41247e9e9f09c4422c82ddcddbb3c56d
hanif365 <m.a.hanifkhan64@gmail.com>
2021-12-19 03:41:57 +0600

    solve CORS error again

--- index.js
+++ index.js
@@ -19,8 +19,7 @@ const port = process.env.PORT || 5000
 
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Z-key");
-    res.header("Content-Type", "application/json");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
     next();
 });
