25af5a42667128c5920b0189ae75b2fe6eb04790
hanif365 <m.a.hanifkhan64@gmail.com>
2021-12-18 12:16:30 +0600

    CORS Policy rollback

--- index.js
+++ index.js
@@ -10,10 +10,7 @@ require('dotenv').config()
 
 const port = process.env.PORT || 5000
 
-app.use(cors({
-    origin: "https://susastho.netlify.app/"
-}));
-// app.use(cors());
+app.use(cors());
 app.use(bodyParser.json());
 
 
