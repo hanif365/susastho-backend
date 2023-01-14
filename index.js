33c4dcd11185ed49e207ab453237415f0d146722
hanif365 <m.a.hanifkhan64@gmail.com>
2023-01-14 21:21:18 +0600

    CORS

--- index.js
+++ index.js
@@ -20,10 +20,20 @@ const port = process.env.PORT || 5000
 // app.use(cors({ origin: true }));
 
 // new cors code
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
+// app.use((req, res, next) => {
+//     res.header('Access-Control-Allow-Origin', '*');
+//     next();
+// });
+
+// 
+
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
     next();
-});
+ });
 // end
 
 
